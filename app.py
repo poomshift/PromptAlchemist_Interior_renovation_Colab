0e8c6f4d003ff9aa4e9f6c3c604134166560864f
promptalchemist <130088861+poomshift@users.noreply.github.com>
2024-04-28 14:56:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -103,9 +103,9 @@ def generate_image(
     return images
 
 # Create the Gradio interface
-with gr.Blocks(title="Interior Renovation Ai") as demo:
-    gr.Markdown("""# Interior Renovation Ai""")   
-    gr.Markdown("โปรแกรมสำหรับรีโนเวทห้องด้วย Ai | fb.com/PromptAlchemist")  
+with gr.Blocks(title="Exterior and Interior Renovation Ai") as demo:
+    gr.Markdown("""# Exterior and Interior Renovation Ai""")   
+    gr.Markdown("โปรแกรมรีโนเวทบ้านและห้องด้วย Ai | fb.com/PromptAlchemist")  
     with gr.Row():
         prompt = gr.Textbox(lines=1, placeholder="เขียนพรอมต์ที่ต้องการเช่น Living room", label="Prompt")
         negative_prompt = gr.Textbox(lines=1, value="Low quality, bad quality, worst quality, 3d, cartoon, painting", label="Negative Prompt")
